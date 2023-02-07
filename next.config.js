ca4db6222bde7490e1b514e827476014cf933ce0
LucasMarson <lucas-marson@hotmail.com>
2023-02-07 14:48:15 -0300

    commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-
 /** @type {import('next').NextConfig} */
+const path = require('path');
+exports.webpack = (config) => {
+  config.resolve.alias['static'] = path.join(__dirname, 'static');
+};
+
 const nextConfig = {
   reactStrictMode: true,
-};
+  env: {
+    GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS
+  }
+}
 
-module.exports = {
-  ...nextConfig,
-};
+module.exports = nextConfig
